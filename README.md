723336bcea1e9276180ee87787a5697033081933
zhaotianzeng <tianzneg@gmail.com>
2022-08-03 19:59:35 +0800

    add 掘金

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # 导出平台内容到EPUB
 
-## 语雀
-
+## 安装虚拟环境和依赖
 ```shell
 git clone https://github.com/zxcvbnmzsedr/py_epub.git
 cd py_epub
+python3 -m venv penv
+cd penv && source ./bin/activate && cd ..
+python3 install -r requirements.txt
+```
+## 语雀
+
+```shell
+
 python yuque_epub.py -c cookie -n 名称 语雀知识库的url地址
+```
+
+## 掘金
+
+```shell
+
+python juejing_epub.py -c cookie -n 名称 book_id
 ```
---
+++ juejing_epub.py
@@ -0,0 +1,113 @@
+import argparse
+import re
+import uuid
+
+import requests
+from bs4 import BeautifulSoup
+from ebooklib import epub
+
+
+def add_book(cookie, book_id, book):
+    # 获取书籍目录
+    result = requests.post(
+        url='https://api.juejin.cn/booklet_api/v1/booklet/get',
+        json={
+            'booklet_id': book_id
+        }
+    ).json()
+    # 生成目录
+    for session in result['data']['sections']:
+        section_id = session['section_id']
+        title = session['title']
+        title = title.replace('/', '_')
+        print(f'get section {title}   {section_id}')
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'cookie': cookie
+        }
+        # 生成章节
+        section_info = requests.post(
+            url='https://api.juejin.cn/booklet_api/v1/section/get',
+            json={
+                'section_id': section_id
+            },
+            headers=headers
+        ).json()
+        section = section_info['data']['section']
+        content = section['content']
+        soup = BeautifulSoup(content, "html.parser")
+
+        for img in soup.select('img'):
+            img_src = img.attrs['src']
+
+            img_id = str(uuid.uuid1())
+            r = requests.get(img_src)
+            # open(doc_path + "/" + img_id, 'wb').write(r.content)
+            img['src'] = img_id
+            epub_image = epub.EpubImage()
+            epub_image.content = r.content
+            epub_image.id = img_id
+            epub_image.file_name = img_id
+            epub_image.media_type = 'image/jpg'
+            book.add_item(epub_image)
+
+        s = html_template(f'<h1>{title}</h1>{html_template(soup.prettify())}')
+        # 修改html中的图片，整到本地环境中
+        c1 = epub.EpubHtml(title=title, content=s, file_name=section_id + '.xhtml', lang='en')
+        # add chapter
+        book.add_item(c1)
+        book.spine.append(c1)
+        book.toc.append(c1)
+
+
+def html_template(html):
+    template = open("./yuyuqe.html", 'rb').read().decode()
+    h = template.replace('{0}', html)
+
+    def filter_emoji(desstr, restr=''):
+        # 过滤表情
+        try:
+            co = re.compile(u'[\U00010000-\U0010ffff]')
+        except re.error:
+            co = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
+        return co.sub(restr, desstr)
+
+    return filter_emoji(h)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-c', '--cookie',
+                        help='Cookie')
+
+    parser.add_argument('-n', '--name',
+                        help='Name the docset explicitly')
+    parser.add_argument('-p', '--index-page',
+                        help='Set the file that is shown')
+    parser.add_argument('book_id',
+                        help='Directory containing the HTML documents')
+    results = parser.parse_args()
+
+    book = epub.EpubBook()
+
+    book.set_identifier(results.name)
+    book.set_title(results.name)
+    book.set_language('cn')
+
+    book.add_author(results.name)
+    book.spine.append('nav')
+    add_book(results.cookie, results.book_id, book)
+
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+
+    # define CSS style
+    style = 'BODY {color: white;}'
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+
+    # add CSS file
+    book.add_item(nav_css)
+
+    # write to the file
+    epub.write_epub(results.name + '.epub', book, {})
